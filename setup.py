2fb01df9016f73115a215de6882900b072304779
David Winterbottom <david.winterbottom@gmail.com>
2012-08-22 15:24:18 +0100

    Add MIT license

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rickroll',
-    version='0.1',
-    description='Rickrolling middleware to annoy potential hackers',
+    version='0.1.1',
+    description='Rickrolling Django middleware to annoy potential hackers',
     long_description=open('README.rst').read(),
     author='David Winterbottom',
     author_email='david.winterbottom@gmail.com',
     url='https://github.com/codeinthehole/django-rickroll',
-    license='BSD',
+    license=open('LICENSE').read(),
     packages=find_packages(exclude=('tests',)),
     tests_require=['django>=1.3'])
